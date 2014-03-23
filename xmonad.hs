5907d974582827cbb12a998152c598e0e7713468
Matus Kmit <matus.kmit@zhdk.ch>
2014-03-23 12:13:55 +0100

    apply minimize only to certain layouts

--- xmonad.hs
+++ xmonad.hs
@@ -114,7 +114,7 @@ startupWorkspace = "5:Dev"  -- which workspace do you want to be on after launch
 -- "avoidStruts" modifier makes it so that the layout provides
 -- space for the status bar at the top of the screen.
 -- "minimize" modifier makes it possible to minimize windows.
-defaultLayouts = minimize(smartBorders(avoidStruts(
+defaultLayouts = smartBorders(avoidStruts(
   -- Full layout makes every window full screen. When you toggle the
   -- active window, it will bring the active window to the front.
   noBorders Full
@@ -123,12 +123,12 @@ defaultLayouts = minimize(smartBorders(avoidStruts(
   -- and remaining windows tile on the right. By default each area
   -- takes up half the screen, but you can resize using "super-h" and
   -- "super-l".
-  ||| ResizableTall 1 (3/100) (1/2) []
+  ||| minimize (ResizableTall 1 (3/100) (1/2) [])
 
   -- Mirrored variation of ResizableTall. In this layout, the large
   -- master window is at the top, and remaining windows tile at the
   -- bottom of the screen. Can be resized as described above.
-  ||| Mirror (ResizableTall 1 (3/100) (1/2) []))))
+  ||| minimize (Mirror (ResizableTall 1 (3/100) (1/2) []))))
 
 -- Here we define some layouts which will be assigned to specific
 -- workspaces based on the functionality of that workspace.
